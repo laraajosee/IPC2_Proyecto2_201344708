30ed9827f50776a9fbb84489877c5edc0d296a27
laraajosee <chemanuel94@gmail.com>
2021-03-31 15:14:13 -0600

    31/03

---
+++ graph.py
@@ -0,0 +1,15 @@
+from graphviz import Graph
+
+
+hola = '''<<TABLE>+"\n<TR>"+"\n<TD>left</TD>"+"\n</TR>"+\n</TABLE>>'''
+hola1 = "\n<TR>"
+hola2 = "\n<TD>left</TD>"
+hola3 = "\n</TR>"
+hola4 = "\n</TABLE>>'''"
+h = Graph('html_table')
+h.node('tab', label=hola)
+
+
+print(hola)
+
+h.view()
